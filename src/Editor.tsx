3b9af35b825e53a768ec5086a83f4ecb31ff8175
minamorl <minamorl@users.noreply.github.com>
2023-06-26 04:11:14 +0900

    wip

--- src/Editor.tsx
+++ src/Editor.tsx
@@ -1,50 +1,66 @@
-import React, { useEffect } from 'react';
-import { $getRoot, $getSelection } from 'lexical';
-import { Composer, ContentEditable, RichTextPlugin, MarkdownShortcutPlugin, useComposerContext } from '@lexical/react';
+import { useEffect } from 'react';
+import {$getRoot, $getSelection, EditorState} from 'lexical';
+import {LexicalComposer} from '@lexical/react/LexicalComposer';
+import {PlainTextPlugin} from '@lexical/react/LexicalPlainTextPlugin';
+import {ContentEditable} from '@lexical/react/LexicalContentEditable';
+import {HistoryPlugin} from '@lexical/react/LexicalHistoryPlugin';
+import {OnChangePlugin} from '@lexical/react/LexicalOnChangePlugin';
+import {useLexicalComposerContext} from '@lexical/react/LexicalComposerContext';
+import LexicalErrorBoundary from '@lexical/react/LexicalErrorBoundary';
+import { RichTextPlugin } from '@lexical/react/LexicalRichTextPlugin';
+import {MarkdownShortcutPlugin} from '@lexical/react/LexicalMarkdownShortcutPlugin';
 
 const theme = {
-    // Theme styling goes here
+  // Theme styling goes here
 };
 
-function onChange(editorState) {
-    editorState.read(() => {
-        const root = $getRoot();
-        const selection = $getSelection();
-        console.log(root, selection);
-    });
+function onChange(editorState: EditorState) {
+  editorState.read(() => {
+    const root = $getRoot();
+    const selection = $getSelection();
+    // eslint-disable-next-line no-console
+    console.log(root, selection);
+  });
 }
 
 function MyCustomAutoFocusPlugin() {
-    const [editor] = useComposerContext();
-
-    useEffect(() => {
-        editor.focus();
-    }, [editor]);
-
-    return null;
+  const [editor] = useLexicalComposerContext();
+  useEffect(() => {
+    editor.focus();
+  }, [editor]);
+  return null;
 }
 
 function onError(error: Error) {
-    console.error(error);
+  // eslint-disable-next-line no-console
+  console.error(error);
 }
 
-function Editor() {
-    const initialConfig = {
-        namespace: 'MyEditor',
-        theme,
-        onError,
-    };
+export function Editor() {
+  const initialConfig = {
+    namespace: 'Veil',
+    theme,
+    onError,
+  };
 
-    return (
-        <Composer initialConfig={initialConfig}>
-            <RichTextPlugin
-                contentEditable={<ContentEditable />}
-                placeholder={<div>Enter some text...</div>}
-            />
-            <MarkdownShortcutPlugin />
-            <MyCustomAutoFocusPlugin />
-        </Composer>
-    );
+  return (
+    <LexicalComposer initialConfig={initialConfig}>
+      <PlainTextPlugin
+        contentEditable={<ContentEditable />}
+        placeholder={<div>Enter some text...</div>}
+        ErrorBoundary={LexicalErrorBoundary}
+      />
+      <OnChangePlugin onChange={onChange} />
+      <HistoryPlugin />
+      <MyCustomAutoFocusPlugin />
+      <RichTextPlugin 
+        contentEditable={<ContentEditable />}  
+        placeholder={<div>Enter some text...</div>}  
+        ErrorBoundary={LexicalErrorBoundary} 
+      />
+      <MarkdownShortcutPlugin />
+    </LexicalComposer>
+  );
 }
 
 export default Editor;
