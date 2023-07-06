2f2986a6c87569088e9d839888cd33783a003301
minamorl <minamorl@users.noreply.github.com>
2023-07-07 00:22:11 +0900

    moving structure wip

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import './main.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Editor } from './Editor';
-import ToolbarWidget from './Toolbar';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
